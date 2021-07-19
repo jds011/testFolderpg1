5a776da30083dc2ee62a54f9a47845f49dc0c31b
Jonathan <jsuconota1@gmail.com>
2021-07-19 19:07:44 +0000

    Updare 7/19

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# ---- YOUR APP STARTS HERE ----
+ # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
@@ -11,6 +11,19 @@ app = Flask(__name__)
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods = ["GET","POST"])
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+#---------SAMPLE----------
+# @app.route('/result', methods = ["GET", "POST"])
+# def timeleft():
+
+#     input1 = request.form["month1"]
+#     input2 = request.form["month2"]
+#     input1_int = birthday_countdown1(input1)
+#     input2_int = birthday_countdown1(input2)    
+#     message = abs(input1_int - input2_int)
+    
+    
+#     return render_template("timeleft.html", message = message)
